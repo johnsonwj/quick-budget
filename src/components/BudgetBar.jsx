394b078f7136323bdb0d2892a7ecde23b2acbabf
William Johnson <johns.willj@gmail.com>
2016-11-07 19:46:30 -0800

    Clean up budget bar math a bit

--- src/components/BudgetBar.jsx
+++ src/components/BudgetBar.jsx
@@ -23,22 +23,21 @@ const BudgetBar = (props) => {
 	const difference = (fmi + vmi) - (fme + vme);
 	const positive = difference > 0;
 
-	const stylify = ratio => ({ flex: `0 0 ${ratio * 100}%` });
-
-	const denominator = positive ? (fmi + vmi) : (fme + vme);
-
 	const fd = _.max([0, Math.abs(difference) - (positive ? vmi : vme)]);
 	const vd = Math.abs(difference) - fd;
 
+	const denominator = positive ? (fmi + vmi) : (fme + vme);
+	const stylify = value => ({ flex: `0 0 ${(value / denominator) * 100}%` });
+
 	return (
 		<div id="budget-bar" className={positive ? 'positive' : 'negative'}>
-			<div id="fixed-income" style={stylify(fmi / denominator)} />
-			<div id="variable-income" style={stylify(vmi / denominator)} />
-			<div id="fixed-expense" style={stylify(fme / denominator)} />
-			<div id="variable-expense" style={stylify(vme / denominator)} />
+			<div id="fixed-income" style={stylify(fmi)} />
+			<div id="variable-income" style={stylify(vmi)} />
+			<div id="fixed-expense" style={stylify(fme)} />
+			<div id="variable-expense" style={stylify(vme)} />
 			<div id="savings" style={{ flex: '0 0 0%' }} />
-			<div id="fixed-difference" style={stylify(fd / denominator)} />
-			<div id="variable-difference" style={stylify(vd / denominator)} />
+			<div id="fixed-difference" style={stylify(fd)} />
+			<div id="variable-difference" style={stylify(vd)} />
 		</div>
 	);
 };
